e2c8603264c7f2046d3bc76950668f249208aab3
James Westwood <james.westwood@ons.gov.uk>
2021-03-31 11:35:38 +0100

    writing out to csvC:/Users/James/miniconda3/envs/SDMX_qual/python.exe c:/Users/James/programming/sdg-SDMX-data-qualifier/main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# from numpy.core.defchararray import endswith
 import yaml
 import pandas as pd
 import numpy as np
 
 # Load config
 config = yaml.safe_load(open('config.yml'))
-
 
 # reading all the meta data in from url
 meta_url = config['meta_url']
@@ -58,4 +56,10 @@ def check_if_proxies_contain_official():
 # quality check
 check_if_proxies_contain_official()
 
+# cleaning up the national_geo col
+df.national_geographical_coverage = df.national_geographical_coverage.str.replace("nan","None")
 
+# get output filename
+csv_nm = config['outfile']
+# write out to csv
+df.to_csv(csv_nm)
