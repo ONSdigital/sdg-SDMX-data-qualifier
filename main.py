d7875fb038cfdc68c71f074c7dafc6988d3e326f
James Westwood <james.westwood@ons.gov.uk>
2021-04-28 11:34:40 +0100

    Saving work so far on the diagram

--- main.py
+++ main.py
@@ -9,17 +9,30 @@ config = yaml.safe_load(open('config.yml'))
 
 # reading all the meta data in from url
 meta_url = config['meta_url']
-df = pd.read_json(meta_url, orient='index')
+meta_data_df = pd.read_json(meta_url, orient='index')
+
+
+def keep_needed_df_cols(df: pd.DataFrame, required_col_list: list):
+    """Drops uneeded columns from a datframe using a list of the 
+        required columns. 
+
+    Args:
+        df (pd.DataFrame): the dataframe to be changed
+        required_col_list (list): a list of exact column names that 
+            are to be kept
+    """    
+    df = df[required_col_list]
+    return df
 
 # dropping uneeded cols
 required_cols = config['required_cols']
-df = df[required_cols]
+meta_data_df = keep_needed_df_cols(meta_data_df, required_cols)
 
 # Identifying proxy terms
 proxy_terms_list = config['proxy_terms']
 
 # other_info col has "None" in, which needs to be nan
-df.other_info.replace("None", np.nan, inplace=True)
+meta_data_df.other_info.replace("None", np.nan, inplace=True)
 
 # Getting terms into str for regex with OR | 
 def regex_or_str(termslist):
@@ -51,10 +64,10 @@ proxy_terms = regex_or_str(proxy_terms_list)
 # proxy_terms = ''.join(config['proxy_terms']).replace(" ", "|")
 
 # Make proxy boolean mask
-proxy_boolean = df.other_info.str.contains(proxy_terms, na=False, regex=True)
+proxy_boolean = meta_data_df.other_info.str.contains(proxy_terms, na=False, regex=True)
 
 # Create new col, 'proxy_indicator'
-df['proxy_indicator'] = proxy_boolean
+meta_data_df['proxy_indicator'] = proxy_boolean
 
 # Make a check: none of proxy_indicator = True should contain this official sentence
 def check_if_proxies_contain_official():
@@ -64,58 +77,60 @@ def check_if_proxies_contain_official():
         contradiction."""
     official = "Data follows the UN specification for this indicator"
     # Isolate those records that contain the proxy keywords
-    proxies_df = df[df.proxy_indicator==True] 
+    proxies_df = meta_data_df[meta_data_df.proxy_indicator==True] 
     # Make a boolean mask
     official_mask = proxies_df[proxies_df.proxy_indicator].other_info.str.contains(official)
     # Apply mask to proxies_df
-    contractions_list = proxies_df[official_mask].index.to_list()
-    for index_num in contractions_list:
+    contradictions_list = proxies_df[official_mask].index.to_list()
+    for index_num in contradictions_list:
         print(f"""There seem to be contradictory statements in other_info in indicator {index_num}""")
-    return contractions_list
+    return contradictions_list
 
 # quality check
 check_if_proxies_contain_official()
 
 # cleaning up the national_geo col
-df.national_geographical_coverage = df.national_geographical_coverage.str.replace("nan","None")
+meta_data_df.national_geographical_coverage = meta_data_df.national_geographical_coverage.str.replace("nan","None")
 
 # Remove archived indicators
-df = df[~df.index.str.contains("archived")]
+meta_data_df = meta_data_df[~meta_data_df.index.str.contains("archived")]
 
-def get_disag_report():
+def get_disag_report(disag_url):
     """Gets the disagregation report from the URL specified in the config file
         then it changes the indicator names so they are the same as metadata df"""
     # Pulling disagg report
-    disag_url = config['disag_url']
     disag_df = pd.read_csv(disag_url)
     # Alter the indicator names so they match the other df
-    disag_df.Indicator = disag_df.Indicator.str[1:]
+    disag_df.Indicator = disag_df.Indicator.str.lstrip("#")
     return disag_df
 
 # Get the disagregation report
-disag_df = get_disag_report()
+DISAG_URL = config['disag_url']
+disag_df = get_disag_report(DISAG_URL)
 
 # checking if Disaggregations col contains keywords geo_disag_terms
-geo_disag_terms_list = config['geo_disag_terms']
+GEO_DISAG_TERMS_LIST = config['geo_disag_terms']
 # Join terms in list with regex or operator
-geo_disag_terms = regex_or_str(geo_disag_terms_list)
+geo_disag_terms = regex_or_str(GEO_DISAG_TERMS_LIST)
 # Creating boolean
 print("Searching for ", geo_disag_terms)
 disag_boolean = disag_df.Disaggregations.str.contains(geo_disag_terms, regex=True)
 print(disag_boolean.value_counts())
 disag_df['geo_disag'] = disag_boolean   
 
-csv_nm = os.path.join(os.getcwd(),'disag_df.csv')
+csv_nm = os.path.join(os.getcwd(),config["disag_outfile"])
 
 # Drop the now uneeded Disaggregations cols
-disag_df.drop(['Disaggregations', 'Number of disaggregations'], axis=1, inplace=True)
+required_disag_cols = config["required_disag_cols"]
+disag_df = keep_needed_df_cols(disag_df, required_disag_cols)
+
 # Set index and merge on index
 disag_df.set_index("Indicator", inplace=True)
 # Left joining df onto disag_df
-df = df.join(disag_df)
+meta_data_df = meta_data_df.join(disag_df)
 
 # Replacing nans with False in the geo_disag series 
-df.geo_disag.replace(np.nan,False, inplace=True)
+meta_data_df.geo_disag.replace(np.nan,False, inplace=True)
 
 # creating local variable to map for uk coverage
 uk_terms_list = config['uk_terms'] 
@@ -137,7 +152,7 @@ def check_only_uk_data(nat_geo_series, geo_disag_series, uk_terms):
     return False
 
 # Applying the check_only_uk_data to map True/False to new 'only_uk_data' series
-df['only_uk_data'] = (df.apply(lambda x:
+meta_data_df['only_uk_data'] = (meta_data_df.apply(lambda x:
                         check_only_uk_data(x.national_geographical_coverage,
                         x.geo_disag,
                         uk_terms_list),
@@ -146,21 +161,21 @@ df['only_uk_data'] = (df.apply(lambda x:
 # Making UK terms uniform --> United Kingdom
 uk_terms_reg = regex_or_str(uk_terms_list)
 print("Searching for regex string", uk_terms_reg)
-df.national_geographical_coverage = df.national_geographical_coverage.str.replace(uk_terms_reg, "United Kingdom", regex=True)
+meta_data_df.national_geographical_coverage = meta_data_df.national_geographical_coverage.str.replace(uk_terms_reg, "United Kingdom", regex=True)
 
 # Including 8-1-1 by setting proxy to false
-df.loc['8-1-1', 'proxy_indicator'] = False
+meta_data_df.loc['8-1-1', 'proxy_indicator'] = False
 
-# sorting the index of the main df
-df = df.sort_index()
+# sorting the index of the meta data df
+meta_data_df = meta_data_df.sort_index()
 
 print("========================Printing df")
-print(df.head(20))
+print(meta_data_df.head(20))
 
 # get output filename
-csv_nm = os.path.join(os.getcwd(),config['outfile'])
+csv_nm = os.path.join(os.getcwd(),config['meta_outfile'])
 # write out to csv
-df.to_csv(csv_nm)
+meta_data_df.to_csv(csv_nm)
 
 #Get SDMX suitability test
 suit = config['suitability_test']
@@ -175,13 +190,13 @@ for col_nm,col_val in suit.items():
         query_string+=" & "
 
 # make the df of included indicators
-print("Querying df for ", query_string)
-inc_df = df.query(query_string)
+print("Querying meta_data_df for ", query_string)
+inc_df = meta_data_df.query(query_string)
 
 # Manually dropping '13-2-2', '17-5-1', '17-6-1' from df because
 # they have been changed into the 2020 indicators, so we do not want to 
 # consider them for SDMX at this point
-inc_df = inc_df.drop(['13-2-2', '17-5-1', '17-6-1'], axis=0)
+inc_df = inc_df.drop(config["2020indicators"], axis=0)
 
 print(f"The shape of inc_df is {inc_df.shape}")
 
@@ -199,7 +214,7 @@ df_build_dict ={
     "SDMX_concept_name" : np.empty_like(unique_disags)
     }
 # write out the csv
-pd.DataFrame(data=df_build_dict).to_csv("SDG_column_names.csv")
+pd.DataFrame(data=df_build_dict).to_csv(config["sdg_cols_outfile"])
 
 #Ticket #19
 # The excel file should have been manually updated with mappings
